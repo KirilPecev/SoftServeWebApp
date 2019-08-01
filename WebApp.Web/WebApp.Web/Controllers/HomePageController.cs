fce153cec11f121922a13addcd5c1be78f4ac72e
IvayloGugalov <ivaylogugalov@gmail.com>
2019-08-01 18:44:21 +0300

    Now returning Json when creating an event

---
+++ WebApp.Web/WebApp.Web/Controllers/HomePageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Web.Models.Event;
+
+namespace WebApp.Web.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult HomePageView()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult HomePageView(EventBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errorMessages = string.Empty;
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    errorMessages += error;
+                }
+                return BadRequest(errorMessages);
+            }
+            return this.Json(model);
+        }
+    }
+}
