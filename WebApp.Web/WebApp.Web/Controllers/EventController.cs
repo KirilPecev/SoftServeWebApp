a36990b5e65bf72d7c2e8e45ee8150014016f91f
Georgi Georgiev <51886803+GGeorgiew988@users.noreply.github.com>
2019-08-04 20:48:45 +0300

    Add files via upload

---
+++ WebApp.Web/WebApp.Web/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult ViewEvent()
+        {
+            return View();
+        }
+    }
+}
