458c1adf6d9ea9a915b10343aa9d25dbd0ef4e5b
DKatsarski <dd.katsarski@gmail.com>
2019-08-08 19:32:22 +0300

    BaseService Implemented

---
+++ WebApp.Web/WebApp.Services/AdminEvent/AdminEventService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Data.Repo;
+
+namespace WebApp.Services.AdminEvent
+{
+    public class AdminEventService : BaseService, IAdminEventService
+    {
+        public AdminEventService(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+
+        }
+    }
+}
---
+++ WebApp.Web/WebApp.Services/AdminEvent/IAdminEventService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApp.Services.AdminEvent
+{
+    interface IAdminEventService
+    {
+    }
+}
