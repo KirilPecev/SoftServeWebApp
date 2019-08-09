ae5a4991c742fe0f82132e08314f0d4700fc467e
valiusa <valentinvladimirov@abv.bg>
2019-08-09 17:30:40 +0300

    Rating Repo
    And Some changes to profile page

--- WebApp.Web/WebApp.Web/Controllers/UsersController.cs
+++ WebApp.Web/WebApp.Web/Controllers/UsersController.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Domain;
+using WebApp.Web.Models;
 
 namespace WebApp.Web.Controllers
 {
     public class UsersController : Controller
     {
+        private UserManager<WebAppUser> _userManager;
+        public UsersController(UserManager<WebAppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Profile()
         {
-            return View();
+            string currentUser = User.Identity.Name;
+
+            UserBindingModel model = new UserBindingModel(currentUser,
+                new List<UserScore>() {
+                     new UserScore ()
+                     {
+                         CurrentDate = DateTime.Now.ToShortDateString(),
+                         Score = 5
+                     },
+                     new UserScore ()
+                     {
+                         CurrentDate = DateTime.Now.ToShortDateString(),
+                         Score = 2.5
+                     },
+                     new UserScore ()
+                     {
+                         CurrentDate = DateTime.Now.ToShortDateString(),
+                         Score = 0.5
+                     }
+            });
+
+            return View("Profile", model);
         }
     }
 }
