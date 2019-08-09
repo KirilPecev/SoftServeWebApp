ab14b4ab263e57d4acfd06d3e8a3ad20ae6104e9
DKatsarski <dd.katsarski@gmail.com>
2019-08-09 19:28:55 +0300

    Skeleton For the EventAttendanceService -> implemented. Also -> EventAttendance repo -> done

---
+++ WebApp.Web/WebApp.Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using WebApp.Data.Repo;
+
+namespace WebApp.Services
+{
+    public class BaseService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public BaseService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _unitOfWork.SaveChanges();
+        }
+    }
+}
---
+++ WebApp.Web/WebApp.Services/EventAttendance/IEventAttendanceService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Data.Repo;
+using WebApp.Domain;
+
+namespace WebApp.Services.EventAttendance
+{
+    public interface IEventAttendanceService
+    {
+    }
+}
