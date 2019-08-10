417955a8f9218a0fdb211b1d8b8f6f7a0be0dea3
Georgi Georgiev <georgi_tr._georgiev@abv.bg>
2019-08-10 20:06:59 +0300

    Massive Update

---
+++ WebApp.Web/WebApp.Data/Repo/EventRepo/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Data.Repo
+{
+    using Domain;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IEventRepository
+    {
+        void CreateEvent(Event createEvent); 
+
+        IEnumerable<Event> GetAllEvents();
+
+        Event GetEvent(int id);
+    }
+}
