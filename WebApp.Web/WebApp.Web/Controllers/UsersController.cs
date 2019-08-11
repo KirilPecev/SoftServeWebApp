0bc805b87d4e3beadea2bd176e9371df1e8399d7
valiusa <valentinvladimirov@abv.bg>
2019-08-11 15:24:48 +0300

    Updates

--- WebApp.Web/WebApp.Web/Controllers/UsersController.cs
+++ WebApp.Web/WebApp.Web/Controllers/UsersController.cs
@@ -3,38 +3,34 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Domain;
+using WebApp.Services.RatingService;
 using WebApp.Web.Models;
+using System.Linq;
 
 namespace WebApp.Web.Controllers
 {
     public class UsersController : Controller
     {
         private UserManager<WebAppUser> _userManager;
-        public UsersController(UserManager<WebAppUser> userManager)
+        private IRatingService ratingService;
+
+        public UsersController(UserManager<WebAppUser> userManager, IRatingService _ratingService)
         {
             _userManager = userManager;
+            ratingService = _ratingService;
         }
 
         public IActionResult Profile()
         {
             string currentUser = User.Identity.Name;
+            double _score = ratingService.GetAllRatings().Where(r => r.Receiver.UserName == currentUser).First().Score;
 
             UserBindingModel model = new UserBindingModel(currentUser,
                 new List<UserScore>() {
                      new UserScore ()
                      {
                          CurrentDate = DateTime.Now.ToShortDateString(),
-                         Score = 1
-                     },
-                     new UserScore ()
-                     {
-                         CurrentDate = DateTime.Now.ToShortDateString(),
-                         Score = 2.5
-                     },
-                     new UserScore ()
-                     {
-                         CurrentDate = DateTime.Now.ToShortDateString(),
-                         Score = 0.5
+                         Score = _score
                      }
             });
 
