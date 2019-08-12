5b540f1d722dedf116a8ddf4674226014403563e
Georgi Georgiev <georgi_tr._georgiev@abv.bg>
2019-08-12 16:05:32 +0300

    fixes

--- WebApp.Web/WebApp.Web/Controllers/AdminEventController.cs
+++ WebApp.Web/WebApp.Web/Controllers/AdminEventController.cs
@@ -9,9 +9,13 @@ using WebApp.Web.Models.Event;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 using WebApp.Services.EventAttendance;
+using System;
+using WebApp.Scheduler.Scheduler;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp.Web.Controllers
-
+{ 
     public class AdminEventController : Controller
     {
         private readonly UserManager<WebAppUser> _userManager;
@@ -21,7 +25,8 @@ namespace WebApp.Web.Controllers
         private readonly IServiceProvider provider;
         private readonly IServiceScopeFactory factory;
 
-        public AdminEventController(IServiceProvider provider, IServiceScopeFactory factory, UserManager<WebAppUser> userManager, IEventService eventService, IEventMapper eventMapper)
+        public AdminEventController(IServiceProvider provider, IServiceScopeFactory factory, 
+            UserManager<WebAppUser> userManager, IEventService eventService, IEventMapper eventMapper, IEventAttendanceService attendanceService)
         {
             this._userManager = userManager;
             this._eventService = eventService;
